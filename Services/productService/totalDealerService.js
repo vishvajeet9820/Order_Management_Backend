1474a372304f9a502677d7688dc3d04d73d97f30
VISHVAJEET SINGH GOUR <59209961+vishvajeet9820@users.noreply.github.com>
2022-04-02 02:13:03 +0530

    Create totalDealerService.js

---
+++ Services/productService/totalDealerService.js
@@ -0,0 +1,19 @@
+async function totalBill() {
+    const productModel = require('../Models/productModel/dealerCartModel');
+    const collect = await productModel.find();
+    var sum = 0;
+    var modelType = {};
+    for (let i = 0; i < collect.length; i++) {
+        sum += (collect[i].discountPrice * collect[i].Quantity);
+        if (collect[i].productType in modelType){
+            modelType[collect[i].productType] = ++modelType[collect[i].productType];
+        } else {
+            modelType[collect[i].productType] = 1;
+        }
+    }
+    result = {amount : sum , typequant : modelType}
+    return result;
+}
+
+
+module.exports = {totalBill};
