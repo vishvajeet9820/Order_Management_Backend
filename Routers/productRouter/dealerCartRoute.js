19e2339e7c3f83a8dd46eae0c2d86fd058261b41
VISHVAJEET SINGH GOUR <59209961+vishvajeet9820@users.noreply.github.com>
2022-04-02 03:22:44 +0530

    Create dealerCartRoute.js

---
+++ Routers/productRouter/dealerCartRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const controller2 = require('../Controllers/productController/dealerCartController')
+router2 = express.Router();
+router2.route("")
+.get((req , res) => {
+    controller2.productAddedDetails(req , res);
+})
+.post((req , res) => {
+    controller2.productAdded(req , res);
+})
+.patch((req , res) => {
+    controller2.increaseQuantity(req , res);
+})
+.delete((req , res) => {
+    controller2.productRemoved(req , res);
+})
+
+module.exports = router2;
