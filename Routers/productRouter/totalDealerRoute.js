ea5ef1d4587fee50bc524acb2aca01b32bbadc98
VISHVAJEET SINGH GOUR <59209961+vishvajeet9820@users.noreply.github.com>
2022-04-02 03:27:01 +0530

    Create totalDealerRoute.js

---
+++ Routers/productRouter/totalDealerRoute.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const controller3 = require('../Controllers/productController/totalDealerController')
+router3 = express.Router();
+router3.route("")
+.get((req , res) => {
+    controller3.totalBill(req , res);
+})
+
+
+module.exports = router3;
